8155dbeac9314edd0c9eee693fd6291f751a8459
A. Jablonszky <farinae.subactae@gmail.com>
2017-11-30 23:22:36 +0100

    end of 'score(game)' is rebuild and creating the new x_or_X(data) function

--- bowling.py
+++ bowling.py
@@ -9,30 +9,29 @@ def score(game):
 
     result = 0
     frame = 1
-    in_first_half = True
+    first_try = True
 
     for i in range(len(game)):
+        result += get_value(game[i])
+
         if game[i] == '/': #ez benne van getvalueban! vmelyikből töröl. nem tud ez lenni a legelső karakter, ezért lehet a last
-            result += 10 - get_value(game[i - 1]) #EZ AZÉRT, HOGY /-NÉL AZ ELŐZŐ ÉRTÉKET TÖRÖLNI, MERT A / = 10
-        else:
-            result += get_value(game[i]) #ez tkp mindenképp fog kelleni
+            result -= get_value(game[i - 1])  # '/': 10 points for 2nd try, but 1st grade needs to be deleted
 
         if frame < 10 and get_value(game[i]) == 10:
             result += get_value(game[i + 1])
-            if game[i] == 'X' or game[i] == 'x':
+            if x_or_X(game[i]):
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1]) #?????
                 else:
                     result += get_value(game[i+2])
 
-        if game[i] == 'X' or game[i] == 'x':
-            frame += 1
-
-        elif in_first_half:
-            in_first_half = False
+        frame += 1
+        if first_try:
+            if not x_or_X(game[i]):
+                frame -= 1
+                first_try = False
         else:
-            frame += 1
-            in_first_half = True
+            first_try = True
 
     return result
 
@@ -58,3 +57,18 @@ def get_value(char):
         return 0
 
     raise ValueError()
+
+
+def x_or_X(data):
+    '''
+    Returns boolean if parameter is "x" or "X"
+
+    Arguments: string
+
+    Returns: boolean
+    '''
+
+    if data == "X" or data == "x":
+        return True
+    else:
+        return False
